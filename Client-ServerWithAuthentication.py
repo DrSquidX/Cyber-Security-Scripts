c1e08f027aeaa052d1f667710a7f72fccbc525c4
DrSquidX <68380622+DrSquidX@users.noreply.github.com>
2021-02-06 10:10:57 +0100

    Update Client-ServerWithAuthentication.py

--- Client-ServerWithAuthentication.py
+++ Client-ServerWithAuthentication.py
@@ -8,20 +8,67 @@ while True:
         break
     except:
         print("\n[+] Invalid IP or Port.\n")
-commands_list = ['!register','!login']
+commands_list = ['!register','!login','!reregister']
 print("[+] Connected to the server.......\n")
+login = False
+register = True
 def instruct():
     while True:
-        instruction = input("[+] What is the instruction: ")
         try:
-            insplit = instruction.split()
-            if insplit[0] in commands_list:
+            global register
+            global login
+            if register:
+                while True:
+                    register_q = input("[+] Do you wish to register for a new account?(yes/no): ")
+                    if register_q.lower() == "yes":
+                        print("[+] You are going to register.\n")
+                        new_user = input("[+] Enter your username: ")
+                        new_pass = input("[+] Enter your password: ")
+                        msg = f"!register {new_user} {new_pass}".encode()
+                        s.send(msg)
+                        servmsg = s.recv(1024).decode()
+                        if "Successfully registered." in servmsg:
+                            print("[+] You have been registered!")
+                            register = False
+                            break
+                        elif "The Username is taken!" in servmsg:
+                            print("[+] The Username has been taken. Try using another.\n")
+                    elif register_q.lower() == "no":
+                        register = False
+                        break
+                    else:
+                        print("[+] Please answer with either 'yes' or 'no'.")
+            if not login:
+                print("\n[+] You are required to login.\n")
+                username = input("[+] Enter your username: ")
+                password = input("[+] Enter your password: ")
+                msg = f"!login {username} {password}".encode('utf-8')
+                s.send(msg)
+                servmsg = s.recv(1024).decode()
+                if "Password Accepted!" in servmsg:
+                    login = True
+                    print(f"\n[+] Successfully logged in as: {username}\n")
+                    reciever = threading.Thread(target=recv)
+                    reciever.start()
+                elif "Password Not Accepted!" in servmsg:
+                    login = False
+                    print("[+] The password you provided is invalid.")
+                elif "Your Username Does Not Exist" in servmsg:
+                    print("[+] The username you provided is invalid. Try to register.\n")
+                    register = True
+            elif login:
+                instruction = input("[+] What is the instruction: ")
                 try:
-                    s.send(instruction.encode('utf-8'))
+                    insplit = instruction.split()
+                    if insplit[0] in commands_list:
+                        try:
+                            s.send(instruction.encode('utf-8'))
+                        except:
+                            print("[+] Your Connection has been Terminated by the host.")
+                            input("\n[+] Press 'enter' to exit.")
+                            quit()
                 except:
-                    print("[+] Your Connection has been Terminated by the host.")
-                    input("\n[+] Press 'enter' to exit.")
-                    quit()
+                    print("[+] Your message is invalid!")
         except:
             pass
 def recv():
@@ -35,6 +82,4 @@ def recv():
         except:
             pass
 sender = threading.Thread(target=instruct)
-reciever = threading.Thread(target=recv)
 sender.start()
-reciever.start()
